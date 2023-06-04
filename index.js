f9977d0412e0e096ff6d5216d6e22d52e800859b
5haddam <131292268+5haddam@users.noreply.github.com>
2023-06-04 21:31:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,12 @@ const dcHeroes = [
 ];
 
 Array.prototype.heroesRender = function(pathName) {
-  const TRs = []
-  for (let i = 0; i < this.length; i++) {
-    const TDs = [];
-    TDs.push(`<td>${this[i].name}</td>`)
-    TDs.push(`<td><img src="images/${pathName}/${this[i].name.toLowerCase().replaceAll(' ', '')}.svg" wigth=50px, height=50px></td>`)
-    TRs.push(`<tr>${TDs.join('')}</tr>`)
-  }
+  const TRs = this.map(function(item) {
+	const TDs = [];
+    TDs.push(`<td>${item.name}</td>`);
+    TDs.push(`<td><img src="images/${pathName}/${item.name.toLowerCase().replaceAll(' ', '')}.svg" wigth=50px, height=50px></td>`);
+	return `<tr>${TDs.join('')}</tr>`;
+  })
   return document.write(`
     <table>
       <thead><tr><th width=200px>Name</th><th width=200px>Icon</th></tr></thead>
